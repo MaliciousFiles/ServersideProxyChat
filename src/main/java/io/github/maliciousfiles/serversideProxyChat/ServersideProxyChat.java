be5f8d1663823f0f1e2255035f2af165075b3cdc
Malcolm Roalson <mtroalson@gmail.com>
2024-12-23 09:53:57 -0600

    initial commit

---
+++ src/main/java/io/github/maliciousfiles/serversideProxyChat/ServersideProxyChat.java
@@ -0,0 +1,17 @@
+package io.github.maliciousfiles.serversideProxyChat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServersideProxyChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
